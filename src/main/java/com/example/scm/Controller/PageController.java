0f23bf0b99c07e30f2e1664113f8f9c841319bde
Shivam <shivamtamboli66@gmail.com>
2025-02-07 08:31:23 +0530

    add attributes to contoller class

--- src/main/java/com/example/scm/Controller/PageController.java
+++ src/main/java/com/example/scm/Controller/PageController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-    @RequestMapping("/home")
-    public String home(Model model){
+    @RequestMapping("/home")//it means when a user visits , on localhost:8081/home
+    public String home(Model model){// then this method will start executing and sending data to html page.
         System.out.println("Home page Controller");
+        //Sending data to view.
         model.addAttribute("name", "SubString Technologies" );
         model.addAttribute("email", "shivamt12345@gmail.com");
         model.addAttribute("password", 1234);
-        return "home";
+        model.addAttribute("Githubrepo", "https://github.com/Shivam-tamboli?tab=overview&from=2025-02-01&to=2025-02-07");
+        return "home";//tell spring boot to show home.html .
     }
 }
